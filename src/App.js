32490bf6fd2338bceebaf574fc19a97578054bb5
sijothomas <sijo.mt@accubits.com>
2022-12-31 19:13:35 +0530

    nineth commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Happy NewYear sijo
+          Happy New Year
         </a>
       </header>
     </div>
