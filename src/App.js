94812707d91d50fd84c1f35c38ffb5d6b3a736fd
sijothomas <sijo.mt@accubits.com>
2022-12-31 18:49:37 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Happy Christmas
+          Happy NewYear
         </a>
       </header>
     </div>
